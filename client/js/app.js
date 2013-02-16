a4b37d8f352906aad5fcb6437193a11c02dcc443
Nathan Hibner <nathan.hibner@lextech.com>
2013-02-16 04:31:59 -0600

    Save images to server.

--- client/js/app.js
+++ client/js/app.js
@@ -4,5 +4,47 @@
 // App Template
 
 Template.app.events({
-	'click #btn-start-recording': function() { startVideoStream(); }
+	'click #btn-start-recording': function() { startVideoStream(); },
+	'click #btn-send-image': function() { saveImage(); }
 });
+
+function saveImage(dataURL) {
+	var blob = getBlob(); // UNTIL PASSED ACTUAL DATAURL
+	//var blob = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+	Meteor.call('saveFile', blob, genFilePath(), "", "base64");
+}
+
+// TEST FUNCTION
+function getBlob() {
+	var img = document.getElementById('file');
+
+    // Create an empty canvas element
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    // Copy the image contents to the canvas
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    // Get the data-URL formatted image
+    // Firefox supports PNG and JPEG. You could check img.src to
+    // guess the original format, but be aware the using "image/jpg"
+    // will re-encode the image.
+    var dataURL = canvas.toDataURL("image/png");
+    //return dataURL;
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
+function randomString(length) {
+    var result = '';
+    var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (var i = length; i > 0; --i) {
+    	result += chars[Math.round(Math.random() * (chars.length - 1))];
+    }
+    return result;
+}
+
+function genFilePath() {
+	var result = Meteor.users().services.google.id + '/' randomString(40) + '.png';
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,57 @@
 // Allow the client access to their Google login data
 // Needed for Google integration
 Meteor.publish("users", function () {
-  return Meteor.users.find({_id: this.userId},
-                           {fields: {'services': 1, 'profile': 1}});
+  	return Meteor.users.find({_id: this.userId}, 
+  		{fields: {'services': 1, 'profile': 1}});
+});
+
+// Server-side methods
+Meteor.methods({
+
+	// Simple function to send an email
+  	sendEmail: function (to, from, subject, html) {
+	    // Let other method calls from the same client start running,
+	    // without waiting for the email sending to complete.
+	    this.unblock();
+
+	    Email.send({
+	      	to: to,
+	      	from: from,
+	      	subject: subject,
+	      	html: html
+	    });
+  	},
+
+  	// Function to save an image file
+  	saveFile: function(blob, name, path, encoding) {
+	    var path = cleanPath(path),
+	    	  fs = __meteor_bootstrap__.require('fs'),
+	        name = cleanName(name || 'file'),
+	    encoding = encoding || 'binary',
+	      chroot = Meteor.chroot || 'public';
+
+	    // Clean up the path. Remove any initial and final '/' -we prefix them-,
+	    // any sort of attempt to go to the parent directory '..' and any empty directories in
+	    // between '/////' - which may happen after removing '..'
+	    path = chroot + (path ? '/' + path + '/' : '/');
+	    
+	    // TODO Add file existance checks, etc...
+	    fs.writeFile(path + name, blob, encoding, function(err) {
+	      if (err) {
+	        throw (new Meteor.Error(500, 'Failed to save file.', err));
+	      } else {
+	        console.log('The file ' + name + ' (' + encoding + ') was saved to ' + path);
+	      }
+	    }); 
+	 
+	    function cleanPath(str) {
+	      if (str) {
+	        return str.replace(/\.\./g,'').replace(/\/+/g,'').
+	          replace(/^\/+/,'').replace(/\/+$/,'');
+	      }
+	    }
+	    function cleanName(str) {
+	      return str.replace(/\.\./g,'').replace(/\//g,'');
+	    }
+	  }
 });
