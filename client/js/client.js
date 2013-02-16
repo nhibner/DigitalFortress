3343f26084e5c6b7cdc2191f5d3b8ad6fd11a2ef
Nathan Hibner <nathan.hibner@lextech.com>
2013-02-15 23:33:02 -0600

    Modified login flow. Added "app" template.

--- client/js/client.js
+++ client/js/client.js
@@ -6,3 +6,14 @@ Accounts.config({
 	sendVerificationEmail: false,
 	forbidClientAccountCreation: true
 });
+
+// Control login flow
+Meteor.autorun(function () {
+  if (Meteor.userId()) {
+    // on login
+    Meteor.Router.to('/app');
+  } else {
+    // on logout
+    Meteor.Router.to('/');
+  }
+});
--- client/js/router.js
+++ client/js/router.js
@@ -6,4 +6,22 @@
 // All routes for Digital Fortress
 Meteor.Router.add({
 	'/': 'home',
+	'/app': 'app'
 });
+
+// Specific filters for routes
+Meteor.Router.filters({
+
+	// Forces user to login before viewing page
+	'requireLogin': function(page) {
+
+		if (Meteor.user()) {
+      		return page;
+    	} else {
+      		return 'home';
+    	}
+	}
+});
+
+// Always force login when attempting to access app template
+Meteor.Router.filter('requireLogin', {only: 'app'});
