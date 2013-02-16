491f19233557b83c157a7127f9222760ad52d5e0
Nathan Hibner <nathan.hibner@lextech.com>
2013-02-16 05:21:05 -0600

    Removed test blob code.

--- client/js/app.js
+++ client/js/app.js
@@ -4,44 +4,20 @@
 // App Template
 
 Template.app.events({
-	'click #btn-start-recording': function() { startVideoStream(); },
-	'click #btn-send-image': function() { saveImage(); }
+	'click #btn-start-recording': function() { startVideoStream(); }
 });
 
 function saveImage(dataURL) {
-	var blob = getBlob(); // UNTIL PASSED ACTUAL DATAURL
-	//var blob = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+	var blob = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 	var filename = genFileName();
 	var filepath = genFilePath();
 	Meteor.call('saveFile', blob, filename, filepath, "base64");
 }
 
-// TEST FUNCTION
-function getBlob() {
-	var img = document.getElementById('file');
-
-    // Create an empty canvas element
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    // Copy the image contents to the canvas
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-
-    // Get the data-URL formatted image
-    // Firefox supports PNG and JPEG. You could check img.src to
-    // guess the original format, but be aware the using "image/jpg"
-    // will re-encode the image.
-    var dataURL = canvas.toDataURL("image/png");
-    //return dataURL;
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
-
 function genFilePath() {
 	// var result = Meteor.user().services.google.id + '';
-	// return result;
-	return 'captures';
+	result = 'captures';
+	return result;
 }
 
 function genFileName() {
