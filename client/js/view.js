dfec8127e56c3b7d67850964c3f293b955072a78
Shugo <shugotanaka@gmail.com>
2013-04-12 00:57:04 -0500

    bg textures

--- client/js/view.js
+++ client/js/view.js
@@ -39,34 +39,23 @@ Template.view.helpers({
 ///////////////////////////////////////////////////////////////////////////////
 // View Template - Events
 
+$.easing.def = "easeOutBounce";
+
 Template.view.events({
 
 	'click .session-nav-link': function(event) {
 		var id = event.target.id;
 		DF.setViewSessionId(id);
 	}
-});
-
-//////////////////////////////////////////////////////////////////////////////
-// Colorful Accordion JS
-$(document).ready(function(){
-	/* This code is executed after the DOM has been completely loaded */
-
-	/* Changing thedefault easing effect - will affect the slideUp/slideDown methods: */
-	$.easing.def = "easeOutBounce";
-
-	/* Binding a click event handler to the links: */
-	$('li.button a').click(function(e){
-	
+	// Colorful Accordion
+	'click li.button a': function(event){
 		/* Finding the drop down list that corresponds to the current section: */
 		var dropDown = $(this).parent().next();
-		
 		/* Closing all other drop down sections, except the current one */
 		/*$('.dropdown').not(dropDown).slideUp('slow');*/
 		dropDown.slideToggle('slow');
 		
 		/* Preventing the default event (which would be to navigate the browser to the link's address) */
-		e.preventDefault();
-	})
-	
+		event.preventDefault();
+	}
 });
