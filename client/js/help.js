64b5aa38d69cb4e515ea8725529be42dc5b8ca91
Nathan Hibner <nathan.hibner@lextech.com>
2013-04-14 21:47:25 -0500

    Started basic layout for the help screen.

--- client/js/help.js
+++ client/js/help.js
@@ -13,5 +13,20 @@ Template.help.helpers({
 
 Template.help.events({
 
-	// Nothing yet
+	// Colorful Accordion
+	'click li.ca-button a': function(event){
+
+		// Use the 'easeOutBounce' animation
+		$.easing.def = "easeOutBounce";
+
+		// Finding the drop down list that corresponds to the current section:
+		var dropDown = $(event.target).parent().next();
+		
+		// Closing all other drop down sections, except the current one
+		// $('.dropdown').not(dropDown).slideUp('slow');
+		dropDown.slideToggle('slow');
+		
+		// Preventing the default event (which would be to navigate the browser to the link's address)
+		event.preventDefault();
+	}
 });
